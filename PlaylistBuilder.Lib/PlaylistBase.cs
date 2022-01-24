cbe837b592f16efca0bbeeda2757879e4b62db06
Chris McGlown <cmcglown@tutamail.com>
2022-01-24 00:05:42 -0500

    Updated random number generator for shuffle method of PlaylistBase

--- PlaylistBuilder.Lib/PlaylistBase.cs
+++ PlaylistBuilder.Lib/PlaylistBase.cs
@@ -95,18 +95,20 @@ public abstract class PlaylistBase : IPlaylist
     }
     public static void Shuffle<T>(IList<T> list)
     {
-        RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
-        int n = list.Count;
-        while (n > 1)
+        using (var provider = RandomNumberGenerator.Create())
         {
-            byte[] box = new byte[1];
-            do provider.GetBytes(box);
-            while (!(box[0] < n * (Byte.MaxValue / n)));
-            int k = (box[0] % n);
-            n--;
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
+            int n = list.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
     }
 }
