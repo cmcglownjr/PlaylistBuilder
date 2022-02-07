143882a2710cd39ddd72a9f99da6df742d0454b3
Chris McGlown <cmcglown@tutamail.com>
2022-02-06 21:24:35 -0500

    Set up some button commands in directory view. Started file navigation logic. Added styles to view. Used ATL to filter media files and playlists.

--- PlaylistBuilder.GUI/ViewModels/DirectoryViewModel.cs
+++ PlaylistBuilder.GUI/ViewModels/DirectoryViewModel.cs
@@ -3,54 +3,144 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Reactive;
 using Avalonia.Controls;
 using PlaylistBuilder.GUI.Models;
 using ReactiveUI;
 using Splat;
+using ATL;
+using ATL.AudioData;
+using ATL.Playlist;
 
 namespace PlaylistBuilder.GUI.ViewModels
 {
     public class DirectoryViewModel : ViewModelBase
     {
-        readonly string _musicDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+        private readonly string _musicDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+        private string _currentDirectory = "";
+        private readonly List<string> _playlistExtensions= new();
+        private readonly List<string> _mediaExtensions = new();
         private IconModel _mediaIconModel;
-        public ObservableCollection<MediaItemModel> ItemList { get; }
+        private List<MediaItemModel> _itemList = new();
+        // public ObservableCollection<MediaItemModel> ItemList { get; private set; }
+        public List<MediaItemModel> ItemList
+        {
+            get => _itemList; 
+            private set => this.RaiseAndSetIfChanged(ref _itemList, value);
+        }
         public IconModel MediaIconModel;
+
+        public string CurrentDirectory
+        {
+            get => _currentDirectory;
+            set => this.RaiseAndSetIfChanged(ref _currentDirectory, value);
+        }
+        public ReactiveCommand<Unit, Unit> HomeBtnPressed { get; }
+        public ReactiveCommand<Unit, Unit> ParentBtnPressed { get; }
+        public ReactiveCommand<Unit, Unit> UndoBtnPressed { get; }
+        public ReactiveCommand<Unit, Unit> RedoBtnPressed { get; }
         public DirectoryViewModel()
         {
             _mediaIconModel = (IconModel)Locator.Current.GetService(typeof(IconModel));
             MediaIconModel = _mediaIconModel;
-            ItemList = new ObservableCollection<MediaItemModel>(PopulateTree(_musicDirectory));
+            FindExtensions();
+            CurrentDirectory = _musicDirectory;
+            ItemList = new List<MediaItemModel>(PopulateTree(_musicDirectory));
+            HomeBtnPressed = ReactiveCommand.Create(HomeDirectory);
+            ParentBtnPressed = ReactiveCommand.Create(ParentDirectory);
+            UndoBtnPressed = ReactiveCommand.Create(UndoNavigation);
+            RedoBtnPressed = ReactiveCommand.Create(RedoNavigation);
         }
-        
+        private void FindExtensions()
+        {
+            foreach (Format f in PlaylistIOFactory.GetInstance().getFormats())
+            {
+                if (f.Readable)
+                {
+                    foreach (string extension in f)
+                    {
+                        _playlistExtensions.Add(extension);
+                    }
+                }
+            }
+            foreach (Format f in AudioDataIOFactory.GetInstance().getFormats())
+            {
+                if (f.Readable)
+                {
+                    foreach (string extension in f)
+                    {
+                        _mediaExtensions.Add(extension);
+                    }
+                }
+            }
+        }
         private List<MediaItemModel> PopulateTree(string directory)
         {
-            List<string> extensions = new List<string> { ".m3u", ".M3U", ".pls", ".PLS", ".xspf", ".XSPF" };
+            
             List<MediaItemModel> itemList = new List<MediaItemModel>();
             DirectoryInfo info = new DirectoryInfo(directory);
             foreach (DirectoryInfo dir in info.GetDirectories())
             {
-                Image mediaImage = new Image();
-                mediaImage.Source = _mediaIconModel.FolderImage;
-                itemList.Add(new MediaItemModel(dir, MediaItemType.Directory, mediaImage));
+                if ((dir.Attributes & FileAttributes.Hidden) == 0)
+                {
+                    Image mediaImage = new Image
+                    {
+                        Source = _mediaIconModel.FolderImage
+                    };
+                    itemList.Add(new MediaItemModel(dir, MediaItemType.Directory, mediaImage));
+                }
             }
 
             foreach (FileInfo file in info.GetFiles())
             {
-                if (extensions.Any(file.Extension.Contains))
+                if (_playlistExtensions.Any(file.Extension.Contains))
                 {
-                    Image mediaImage = new Image();
-                    mediaImage.Source = _mediaIconModel.PlaylistImage;
+                    Image mediaImage = new Image
+                    {
+                        Source = _mediaIconModel.PlaylistImage
+                    };
                     itemList.Add(new MediaItemModel(file, MediaItemType.Playlist, mediaImage));
                 }
-                else
+                else if (_mediaExtensions.Any(file.Extension.Contains))
                 {
-                    Image mediaImage = new Image();
-                    mediaImage.Source = _mediaIconModel.MusicImage;
+                    Image mediaImage = new Image
+                    {
+                        Source = _mediaIconModel.MusicImage
+                    };
                     itemList.Add(new MediaItemModel(file, MediaItemType.Media, mediaImage));
                 }
             }
+
+            CurrentDirectory = directory;
             return itemList;
+        }
+
+        private void HomeDirectory()
+        {
+            string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            ItemList.Clear();
+            ItemList = new List<MediaItemModel>(PopulateTree(homeDirectory));
+            TrackNavigation();
+        }
+
+        private void ParentDirectory()
+        {
+            //TODO: Logic for parent directory
+        }
+
+        private void TrackNavigation()
+        {
+            //TODO: Logic to track navigation changes
+        }
+
+        private void UndoNavigation()
+        {
+            //TODO: Logic to undo navigation choice
+        }
+
+        private void RedoNavigation()
+        {
+            //TODO: Logic to redo navigation choice
         }
     }
 }
