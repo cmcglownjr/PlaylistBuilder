26f4915daf68778cb2cd62c3735363ab67fc09bb
Chris McGlown <eezyville@protonmail.com>
2023-08-15 12:31:25 -0400

    Truncate the length of breadcrumb string

--- PlaylistBuilder.GUI/Models/BreadcrumbModel.cs
+++ PlaylistBuilder.GUI/Models/BreadcrumbModel.cs
@@ -2,10 +2,20 @@ namespace PlaylistBuilder.GUI.Models;
 
 public class BreadcrumbModel
 {
-    public string Text { get; set; }
+    private string _text;
+
+    public string Text
+    {
+        get => Truncate(_text, 8);
+        set => _text = value;
+    }
 
     public BreadcrumbModel(string text)
     {
         Text = text;
+    }
+    private static string Truncate(string s, int max) 
+    { 
+        return s?.Length > max ? s.Substring(0, max) : s ?? throw new ArgumentNullException(s); 
     }
 }
