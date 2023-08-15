346da376c0760222c26fbb801608d88691d4a4ce
Chris McGlown <eezyville@protonmail.com>
2023-08-14 20:03:40 -0400

    Added model for breadcrumb bar

---
+++ PlaylistBuilder.GUI/Models/BreadcrumbModel.cs
@@ -0,0 +1,11 @@
+namespace PlaylistBuilder.GUI.Models;
+
+public class BreadcrumbModel
+{
+    public string Text { get; set; }
+
+    public BreadcrumbModel(string text)
+    {
+        Text = text;
+    }
+}
